cbb49b2e634ba3a35c1ecf90b69c60ba337bbd07
Juan Benjumea <juanbenjumea@gmail.com>
2015-12-11 01:31:16 -0500

    - Crear vista de venta
    - Ajustar tabla de vista de TiendaAbir

--- src/js/controllers/venta_controller.js
+++ src/js/controllers/venta_controller.js
@@ -4,4 +4,16 @@ angular.module('JollyVentasApp.controllers.Venta', [])
     ['$scope', '$rootScope', '$location', 'AuthenticationService',
     function ($scope, $rootScope, $location, AuthenticationService) {
 
+    	$scope.cantidad = 0;
+
+    	$scope.articulo_agregar = function(){
+    		var nuevo_valor = $scope.cantidad + 1;
+			$scope.cantidad = nuevo_valor;
+    	}
+    	$scope.articulo_remover = function(){
+    		if($scope.cantidad > 0){
+	    		var nuevo_valor = $scope.cantidad - 1;
+				$scope.cantidad = nuevo_valor;
+    		}
+    	}
     }]);
