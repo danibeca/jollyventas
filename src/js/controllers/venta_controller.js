0bb6e6bb745b5c4275605f9072db9162fa2b70d0
Juan Benjumea <juanbenjumea@gmail.com>
2015-12-12 15:23:22 -0500

    - Crear vistas de Apertura de tienda y venta

--- src/js/controllers/venta_controller.js
+++ src/js/controllers/venta_controller.js
@@ -5,14 +5,17 @@ angular.module('JollyVentasApp.controllers.Venta', [])
     function ($scope, $rootScope, $location, AuthenticationService) {
 
     	$scope.cantidad = 0;
+    	$scope.total = 0;
 
     	$scope.articulo_agregar = function(){
     		var nuevo_valor = $scope.cantidad + 1;
 			$scope.cantidad = nuevo_valor;
+			$scope.total = $scope.total+1000;
     	}
     	$scope.articulo_remover = function(){
     		if($scope.cantidad > 0){
 	    		var nuevo_valor = $scope.cantidad - 1;
+	    		$scope.total = $scope.total-1000;
 				$scope.cantidad = nuevo_valor;
     		}
     	}
