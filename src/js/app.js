fc82c2cc7403c32cdd226f1b53dfed2269370001
Daniel Betancur <lloploup@gmail.com>
2015-12-13 16:25:25 +1000

    Fixing Navbar

--- src/js/app.js
+++ src/js/app.js
@@ -13,33 +13,40 @@ angular.module('JollyVentasApp', [
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
         .when('/', {
-            redirectTo: '/venta'
+            redirectTo: '/venta',
+            reloadOnSearch: false
         })
         .when('/login', {
             controller: 'LoginController',
-            templateUrl: 'login.html'
+            templateUrl: 'login.html',
+            reloadOnSearch: false
         })
         .when('/tienda-seleccionar', {
             controller: 'TiendaSeleccionarController',
-            templateUrl: 'tienda-seleccionar.html'
+            templateUrl: 'tienda-seleccionar.html',
+            reloadOnSearch: false
         })
         .when('/tienda-abrir', {
             controller: 'TiendaAbrirController',
-            templateUrl: 'tienda-abrir.html'
+            templateUrl: 'tienda-abrir.html',
+            reloadOnSearch: false
         })
         .when('/tienda-cerrar', {
             controller: 'TiendaCerrarController',
-            templateUrl: 'tienda-cerrar.html'
+            templateUrl: 'tienda-cerrar.html',
+            reloadOnSearch: false
         })
         .when('/venta', {
             controller: 'VentaController',
-            templateUrl: 'venta.html'
+            templateUrl: 'venta.html',
+            reloadOnSearch: false
         })
         .when('/solicitud', {
             controller: 'CompraSolicitudController',
-            templateUrl: 'solicitud.html'
+            templateUrl: 'solicitud.html',
+            reloadOnSearch: false
         })
-        .otherwise({ redirectTo: '/login' });
+        .otherwise({ redirectTo: '/login' });        
 }])
 .run(['$rootScope', '$location', '$cookieStore', '$http',
     function ($rootScope, $location, $cookieStore, $http) {
@@ -50,7 +57,10 @@ angular.module('JollyVentasApp', [
   
         $rootScope.$on('$locationChangeStart', function (event, next, current) {            
             if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-                $location.path('/login');
+                $location.path('/login');                
+            }
+            if ($location.path() == '/login'){
+                $rootScope.showSideBar = false;
             }
         });
     }]);
